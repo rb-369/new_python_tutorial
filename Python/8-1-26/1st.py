3ad08b80106de9f0967007a3915042c51442058c
rb-369 <rudrababar369@gmail.com>
2026-01-08 09:45:36 +0530

    Added python code to decide if a person is adult or not

--- Python/8-1-26/1st.py
+++ Python/8-1-26/1st.py
@@ -4,8 +4,28 @@ age = 17
 def greet(name, age):
     print("Hi " + name + " Your Age is " + str(age))
 
+def Ask():
+
+
+    age = int(input("Enter your age: "))
+
+    if(age<0):
+        print("Invalid Age")
+        return
+
+    if(age>18 and age<=60):
+        print("You are Adult")
+        if(age>60):
+            print("You are Senior Citizen")
+    elif(age < 18 and age>12):
+        print("You are a Teenager")
+    else:
+        print("You are a Child")            
+
 def main():
     greet("eg", age)
+    Ask()
+
 
 if __name__ == "__main__":
     main()
