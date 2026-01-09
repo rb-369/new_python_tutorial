431742607ab24a82779036edfba9f9dc10cfaa8b
rb-369 <rudrababar369@gmail.com>
2026-01-09 09:34:31 +0530

    Added some more functions

--- Python/8-1-26/1st.py
+++ Python/8-1-26/1st.py
@@ -50,13 +50,54 @@ def oddOrEvenRange():
             print(f"{num} The number is Odd") 
 
 
+#print revesed number
+
+def reverseNumber():
+    num = int(input("Enter a number to reverse: "))
+    reversed_num = 0
+    while(num > 0):
+        digit = num % 10
+        reversed_num = reversed_num * 10 + digit
+        num = num // 10
+    print(f"Reversed Number is: {reversed_num}")
+
+#factorial of a number
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+#print prime numbers in a range
+def primeNumbersInRange():
+    lower = int(input("Enter lower range: "))
+    upper = int(input("Enter upper range: "))
+
+    if(lower > upper):
+        print("Invalid Range! Upper range must be greater than lower range.")
+        return
+
+    if(lower < 0):
+        print("Invalid Range! Lower range must be non-negative.")
+        return
+    
+    print(f"Prime numbers between {lower} and {upper} are:")
+    for num in range(lower, upper + 1):
+        if num > 1:
+            for i in range(2, int(num**0.5) + 1):
+                if (num % i) == 0:
+                    break
+            else:
+                print(num)    
 
 # Main function to execute the script
 def main():
     greet("eg", age)
     # Ask()
-    oddOrEven()
-    oddOrEvenRange()
+    # oddOrEven()
+    # oddOrEvenRange()
+    reverseNumber()
+    primeNumbersInRange()
 
 # Execute main function
 if __name__ == "__main__":
