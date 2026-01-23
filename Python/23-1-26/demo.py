050b0958cf30b5fab3f6a495686ff07ab7625087
rb-369 <rudrababar369@gmail.com>
2026-01-23 08:48:28 +0530

    Added vowels and consonants separator

--- Python/23-1-26/demo.py
+++ Python/23-1-26/demo.py
@@ -19,11 +19,27 @@ def sep():
     print("Even numbers:", even_numbers)
     print("Odd numbers:", odd_numbers)        
 
+def sep2():
+    word = input("Enter a without space word: ")
+    if(' ' in word):
+        print("Invalid input! Please enter a word without spaces.")
+        return
+    vowels = 'aeiouAEIOU'
+    vowel_chars = []
+    consonant_chars = []
+    for char in word:
+        if char in vowels:
+            vowel_chars.append(char)
+        else:
+            consonant_chars.append(char)
 
+    print(f"Total {len(vowel_chars)} Vowels in the word:", vowel_chars)
+    print(f"Total {len(consonant_chars)} Consonants in the word:", consonant_chars)        
 
 def main():
     print("Hello, World!")
-    sep()
+    # sep()
+    sep2()
 
 
 if __name__ == "__main__":
