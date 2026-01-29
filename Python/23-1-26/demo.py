731e9ed14eb3222539034f97631007c06fcb6d9c
rb-369 <rudrababar369@gmail.com>
2026-01-29 08:31:57 +0530

    commited

--- Python/23-1-26/demo.py
+++ Python/23-1-26/demo.py
@@ -34,12 +34,54 @@ def sep2():
             consonant_chars.append(char)
 
     print(f"Total {len(vowel_chars)} Vowels in the word:", vowel_chars)
-    print(f"Total {len(consonant_chars)} Consonants in the word:", consonant_chars)        
+    print(f"Total {len(consonant_chars)} Consonants in the word:", consonant_chars)  
+
+def eg():
+
+    lim = int(input("Enter the limit: "))
+    if(lim < 0):
+        print("Invalid limit! Limit must be non-negative.")
+        return
+    l1 = []
+    for i in range(1, lim+1):
+        l1.append(int(input(f"Enter element {i}: ")))
+
+    print("Original List:", l1)    
+
+    for i in range(len(l1)):
+        if(i != 0):
+            l1[i] = l1[i] + l1[i-1]
+
+    print("Cumulative addition of List:", l1)  
+    str1 = "seriuh" 
+    print(str1[:-1])        
+
+def longest_prefix():
+
+    l1 = []
+    n = int(input("Enter number of strings: "))
+    for i in range(n):
+        s = input(f"Enter string {i+1}: ")
+        l1.append(s)
+
+    longest_prefix = ""    
+
+    for i in range(len(l1[0])):
+        char = l1[0][i]
+        for j in range(1, n):
+            if i >= len(l1[j]) or l1[j][i] != char:
+                return print("Longest Common Prefix:", longest_prefix) if longest_prefix else print("No Common Prefix Found")
+        longest_prefix += char
+        res = longest_prefix  
+
+    
 
 def main():
     print("Hello, World!")
     # sep()
-    sep2()
+    # sep2()
+    # eg()
+    longest_prefix()
 
 
 if __name__ == "__main__":
